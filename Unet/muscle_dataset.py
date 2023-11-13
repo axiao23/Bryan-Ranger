889189d07804801adfef1c2a023fe54d1e35a350
chohk3 <chohk@bc.edu>
2023-11-13 16:11:28 -0500

    update

--- Unet/muscle_dataset.py
+++ Unet/muscle_dataset.py
@@ -5,21 +5,16 @@ from PIL import Image
 import torchvision.transforms.functional as TF
 from sklearn.model_selection import train_test_split
 import dill
-torch.utils.data.get_worker_info().data_loader._AutoCollationBatchReducer = dill.reducer
-
 
 class Muscle(Dataset):
   def __init__(self, train = True, transformX = None, transformY = None):
     # I have previously created a file named 500_train.csv using the file names. Here we will read in the csv to access data in google drive.
     # hayo: should this be 300_train.csv??
+    validation_set_size = 0.2
     self.pixel_file = pd.read_csv('/Users/taliacho/Downloads/Ranger Lab/Bryan-Ranger/local_data/300_train.csv')
     self.transformX = transformX
     self.transformY = transformY
     self.train = train
-    self.complex_object = dill.dumps(your_complex_object)
-
-    validation_set_size = 0.2
-    
     # Split the dataset to train and validation using sklearn function train_test_split
     self.train_data, self.validation_data = train_test_split(self.pixel_file,
                                                                 test_size = validation_set_size,
@@ -31,7 +26,6 @@ class Muscle(Dataset):
 
   def __getitem__(self, index):
     train_path = '/Users/taliacho/Downloads/Ranger Lab/Bryan-Ranger/local_data/train_data'
-    complex_object = dill.loads(self.complex_object)
     if self.train:
       imx_name = os.path.join(train_path, self.train_data.iloc[index, 1])
       imy_name = os.path.join(train_path, self.train_data.iloc[index, 1].replace('.jpeg','_mask.jpg'))
